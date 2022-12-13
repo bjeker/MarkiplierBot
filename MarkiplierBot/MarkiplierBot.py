5ccbcbd4f4fd3b49114312132ce0d5c3e8acd764
Ryan <rbieke1@gmail.com>
2022-12-13 01:03:39 -0600

    Markiplier bot new

---
+++ MarkiplierBot/MarkiplierBot.py
@@ -0,0 +1,73 @@
+# bot .py
+from inspect import getmembers
+import os
+import discord
+import datetime
+import time
+from discord.ext import commands
+from dotenv import load_dotenv
+
+#load variables from .env
+load_dotenv()
+
+#discord token and server names
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+#variable def
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix = '!', intents = intents)
+
+#bot commands
+
+#play audio
+@bot.command()
+async def play(ctx):
+    voice = ctx.voice_client
+    source = await discord.FFmpegOpusAudio.from_probe("markiplier.mp3")
+    voice.play(source)
+
+#join voice channel
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+#leave voice channel
+@bot.command()
+async def leave(ctx):
+    await ctx.guild.voice_client.disconnect()
+
+#command for hello everybody my name is markiplier
+@bot.command()
+async def markiplier(ctx):
+    #define voice channel and seconds for timer
+    channel = ctx.author.voice.channel
+    sec = 6
+
+    #connect and play audio
+    await channel.connect()
+    voice = ctx.voice_client
+    source = await discord.FFmpegOpusAudio.from_probe("markiplier.mp3")
+    voice.play(source)
+
+    #wait till end of audio then leave
+    while sec:
+        time.sleep(1)
+        sec -= 1
+
+    #leave after timer is up
+    await ctx.guild.voice_client.disconnect()
+
+#moderation commands
+
+#timeout deansdale
+@bot.command()
+async def dome(ctx):
+    deansdale = ctx.guild.get_member(800508361719939113)
+    await deansdale.timeout_for(datetime.timedelta(seconds = 40), reason= "being a fuckass")
+    print("deansdale has been ejected for being sus!")
+
+#run the bot
+print("markiplier is connecting...")
+bot.run(TOKEN)
---
+++ README.md
@@ -0,0 +1,3 @@
+# MarkiplierBot
+Hello everybody this is my markiplier python bot
+Making this for fun to use on discord with friends, and to learn how APIs, encoders, and other things work
